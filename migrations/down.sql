e49f0910ee3244fd708020c6c0ba6f510e1d4908
dissatisfied-nerd <sanek.glov@gmail.com>
2023-10-01 00:43:36 +0300

    db edited to BCNF

--- migrations/down.sql
+++ migrations/down.sql
@@ -1,6 +1,10 @@
-drop table products cascade ;
-drop table orders cascade ;
-drop table clients cascade ;
-drop table couriers cascade ;
-drop table stores cascade ;
-drop table products_stores cascade ;
+drop table reviews cascade;
+drop table orders cascade;
+drop table products_categories cascade;
+drop table products cascade;
+drop table categories cascade;
+drop table stores cascade;
+drop table administrators cascade;
+drop table couriers cascade;
+drop table clients cascade;
+drop table addresses cascade;
--- migrations/up.sql
+++ migrations/up.sql
@@ -1,45 +1,87 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS stores
+CREATE TABLE IF NOT EXISTS addresses
 (
-    id   UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name varchar(100)
-    );
-
-CREATE TABLE IF NOT EXISTS couriers
-(
-    id   UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name varchar(100)
-    );
+    id        UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    city      varchar(128),
+    street    varchar(128),
+    building  int,
+    entrance  int,
+    floor     int,
+    apartment int
+);
 
 CREATE TABLE IF NOT EXISTS clients
 (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name varchar(100)
+    id         UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    first_name varchar(128),
+    last_name  varchar(128),
+    balance    float,
+    address_id UUID REFERENCES addresses (id)
+);
 
-    );
-
-CREATE TABLE IF NOT EXISTS orders
+CREATE TABLE IF NOT EXISTS couriers
 (
     id         UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name       varchar(100),
-    courier_id UUID REFERENCES couriers (id),
-    client_id UUID REFERENCES clients(id)
-    );
+    first_name varchar(128),
+    last_name  varchar(128),
+    propiska   boolean
+);
+
+CREATE TABLE IF NOT EXISTS administrators
+(
+    id         UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    first_name varchar(128),
+    last_name  varchar(128)
+);
+
+CREATE TABLE IF NOT EXISTS stores
+(
+    id         UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    reputation float,
+    name       varchar(128)
+);
+
+CREATE TABLE IF NOT EXISTS categories
+(
+    id       UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    category varchar(128)
+);
 
 CREATE TABLE IF NOT EXISTS products
 (
-    id       UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name     varchar(100),
-    price    int,
-    weight   float,
-    order_id UUID REFERENCES orders (id),
-    store_id UUID REFERENCES stores (id)
+    id               UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name             varchar(128),
+    price            float CHECK (price > 0),
+    weight           float,
+    description      varchar(512),
+    image            bytea,
+    store_id         UUID REFERENCES stores (id),
+    administrator_id UUID REFERENCES administrators (id)
 );
 
-CREATE TABLE IF NOT EXISTS products_stores
+CREATE TABLE IF NOT EXISTS products_categories
 (
-    product_id UUID REFERENCES products(id),
-    store_id UUID REFERENCES stores(id),
-    PRIMARY KEY (product_id, store_id)
-)
+    product_id  UUID REFERENCES products (id),
+    category_id UUID REFERENCES categories (id),
+    PRIMARY KEY (product_id, category_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id            UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name          varchar(128),
+    creation_time timestamp,
+    delivery_time timestamp,
+    client_id     UUID REFERENCES clients (id),
+    courier_id    UUID REFERENCES couriers (id),
+    product_id    UUID REFERENCES products (id)
+);
+
+CREATE TABLE IF NOT EXISTS reviews
+(
+    id        UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    client_id UUID REFERENCES clients (id),
+    order_id  UUID REFERENCES orders (id),
+    mark      int
+);

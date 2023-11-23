230b98befaaf456e399430f13d37ce685deb8771
kappaprideonly <kolmanoney@gmail.com>
2023-11-23 14:39:04 +0300

    some improvements

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"context"
+	"delivery-bug/config"
+	router "delivery-bug/internal/http"
 	"delivery-bug/pkg/logging"
-	"delivery-bug/pkg/router"
 	"delivery-bug/pkg/storage/postgres"
 	"fmt"
-	"golang.org/x/sync/errgroup"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-)
 
-const maxConns = 100
+	"github.com/heetch/confita"
+	"github.com/heetch/confita/backend/env"
+	"golang.org/x/sync/errgroup"
+)
 
 var logger = logging.GetLogger()
 
 func main() {
 	ctx := context.Background()
 
-	config, err := postgres.NewPoolConfig()
+	var cfg config.Config
+	err := confita.NewLoader(
+		env.NewBackend(),
+	).Load(ctx, &cfg)
 	if err != nil {
 		logger.Fatal(err)
 	}
-	config.MaxConns = maxConns
 
-	_, err = postgres.ConnectDB(config)
+	logger.Info(cfg)
+
+	_, err = postgres.ConnectDB(cfg.Database)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -49,7 +55,7 @@ func main() {
 	})
 
 	r := router.SetupRoutes()
-	err = http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), r)
 	if err != nil {
 		logger.Fatal(err)
 	}

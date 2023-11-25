8f47e57ae0ed7581d018e2d8309f824d38cca609
dissatisfied-nerd <sanek.glov@gmail.com>
2023-11-25 06:43:53 +0300

    db fix

--- backend/migrations/down.sql
+++ backend/migrations/down.sql
@@ -1,4 +1,4 @@
-drop table basket;
+drop table order_products;
 drop table reviews cascade;
 drop table orders cascade;
 drop table products_stores;
@@ -6,7 +6,8 @@ drop table products_tags cascade;
 drop table products cascade;
 drop table tags cascade;
 drop table stores cascade;
+drop table administrators cascade;
 drop table couriers cascade;
-drop table addresses cascade;
 drop table loginForm cascade;
 drop table clients cascade;
+drop table addresses cascade;

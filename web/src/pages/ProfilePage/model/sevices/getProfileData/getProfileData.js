0ac178868147ce25db7c3c74a9dd802293769234
Знай Артемий <artem.znay@mail.ru>
2023-12-09 13:50:48 +0300

    Add /client/:id request

---
+++ web/src/pages/ProfilePage/model/sevices/getProfileData/getProfileData.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { clientActions } from "entities/Client";
+import { courierActions } from "entities/Courier";
+import { authActions, getAuthType } from "features/Auth";
+
+export const getProfileData = createAsyncThunk(
+    "profile/getProfileData",
+    async (id, thunkAPI) => {
+        const { extra, rejectWithValue, dispatch, getState } = thunkAPI;
+        const type = getAuthType(getState());
+
+        try {
+            const response = await extra.api.get(`api/${type}/${id}`);
+
+            if (!response.data) {
+                throw new Error();
+            }
+
+            if (type === "client") {
+                dispatch(clientActions.setClientData(response.data));
+            } else {
+                dispatch(courierActions.setCourierData(response.data));
+            }
+            dispatch(authActions.saveAuthData(response.data));
+            return response.data;
+        } catch (e) {
+            return rejectWithValue("error");
+        }
+    }
+);

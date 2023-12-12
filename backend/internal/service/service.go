d05812ddfe3aea678e2994e3d02e661da72b7eca
mrnelt <kolmanoney@gmail.com>
2023-12-12 21:38:46 +0300

    delete orders service

--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -3,7 +3,6 @@ package service
 import (
 	"delivery-bug/internal/repo"
 	"delivery-bug/internal/service/courier"
-	"delivery-bug/internal/service/order"
 	"delivery-bug/internal/service/user"
 	"delivery-bug/pkg/logging"
 )
@@ -11,13 +10,11 @@ import (
 type Service struct {
 	UserService    user.UsersService
 	CourierService courier.CouriersService
-	OrderService   order.OrderService
 }
 
 func NewService(repo repo.Repository, l logging.Logger) Service {
 	return Service{
 		UserService:    user.NewService(repo.UserRepo, l),
 		CourierService: courier.NewService(repo.CourierRepo, l),
-		OrderService:   order.NewService(repo.OrderRepo, l),
 	}
 }

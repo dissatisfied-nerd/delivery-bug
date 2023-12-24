d3ff4c4949243f4198bf23bf10fbc18d130cd78c
Arsen Yarullin <arsyarullin@ozon.ru>
2023-12-24 19:19:45 +0300

    fix

--- backend/internal/repo/store/queries.go
+++ backend/internal/repo/store/queries.go
@@ -4,7 +4,7 @@ const (
 	insertAddressQuery = `INSERT INTO addresses(city, street, building, entrance, floor, apartment)
     	VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
 	insertStoreQuery     = `INSERT INTO stores(name, address_id) VALUES($1, $2) RETURNING id`
-	insertLoginFormQuery = `INSERT INTO stores_loginform(login, password, stores_id) VALUES ($1, $2, $3)`
+	insertLoginFormQuery = `INSERT INTO stores_loginform(login, password, store_id) VALUES ($1, $2, $3)`
 	checkLoginQuery      = `SELECT login, password, store_id FROM stores_loginform where login=$1`
 	selectStoreQuery     = `SELECT name, address_id FROM stores WHERE id=$1`
 	selectAddressQuery   = `SELECT city, street, building, entrance, floor, apartment FROM addresses 

7d3e586641092f962fbf5be5ec56590cc9cf4a53
dissatisfied-nerd <sanek.glov@gmail.com>
2023-12-25 04:17:38 +0300

    fix XD

--- backend/internal/ports/router.go
+++ backend/internal/ports/router.go
@@ -55,10 +55,6 @@ func SetupRoutes(service service.Service, repo repo.Repository, logger logging.L
 			router.GET("/products", h.ProductHandler.GetProducts)
 			router.GET("/products/:id", h.ProductHandler.GetProductByID)
 			router.DELETE("/products/:id", h.ProductHandler.DeleteProductById)
-			router.GET("/products/:productID", h.ProductHandler.GetProductByID)
-			router.POST("/products/delete/:id", h.ProductHandler.DeleteProductById)
-			router.GET("/products/:id", h.ProductHandler.GetProductByID)
-			router.DELETE("/products/:id", h.ProductHandler.DeleteProductById)
 		}
 		orders := router.Group("/orders")
 		{
